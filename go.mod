dfdb67482ed32759a67431105d468a33edf6a1e7
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-10-28 20:02:11 +0700

    add login handler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mdayat/capstone-rest-service
 
 go 1.21.3
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/julienschmidt/httprouter v1.3.0
+)
---
+++ internal/routes/auth.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/mdayat/capstone-rest-service/internal/auth"
+)
+
+func Login(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	defer r.Body.Close()
+
+	var tokenString string
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&tokenString)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	claims, err := auth.ValidateGoogleJWT(tokenString)
+	if err != nil {
+		errMsg := "invalid google auth"
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte(errMsg))
+		return
+	}
+
+	fmt.Println(claims.Picture)
+}
